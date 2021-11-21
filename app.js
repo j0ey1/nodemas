dd0088b31352698a077a078dcfc50b266f341309
j0ey1 <71097373+j0ey1@users.noreply.github.com>
2021-11-21 20:31:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send("Welcome to the home page babyy");
-})
-
-app.listen(3000, () => {
-    
-    
+});
+ 
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log("Waazzapppppp");
